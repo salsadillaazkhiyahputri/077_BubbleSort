70eeb12146768da6d40e805fe2e86647c648ea2f
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-03-08 15:57:42 +0700

    menambahkan procedur display

--- 077_BubbleSort/077_BubbleSort.cpp
+++ 077_BubbleSort/077_BubbleSort.cpp
@@ -52,6 +52,30 @@ void bubblesortArray() { //procedur untuk mengurutkan array dengan metode bubble
 
 }
 
+void display() {
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "====================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j]; //menampilkan array
+		if (j < n - 1) {
+			cout << " -->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << "jumlah pass = " << n - 1 << endl; //menampilkan jumlah dari pass
+	cout << endl;
+	cout << endl;
+
+
+
+
+}
+
 int main()
 
 {
