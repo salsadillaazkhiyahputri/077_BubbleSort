1c7f5d363b1c8bc4a29a5e4b466797dde4616a04
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-03-08 15:25:58 +0700

    input

--- 077_BubbleSort/077_BubbleSort.cpp
+++ 077_BubbleSort/077_BubbleSort.cpp
@@ -2,10 +2,30 @@
 #include <string>
 using namespace std;
 
-int arr[20];
+int arr[20]; 
 int n;
 
 
-void input() {
-	int d;
+void input() { //procedur untuk input 
+	while (true) {
+		cout << "Masukkan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukkan elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) { // Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": "; //Output ke layar
+		cin >> arr[i];
+	}
+}
+int main() {
+
 }
